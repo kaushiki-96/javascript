aa11a9079ad4400a68e4cee9167e1000bdfaffa1
kaushiki-96 <kaushiki251996@gmail.com>
2023-12-13 20:57:36 +0530

    new commit

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("hello world!");
+console.log("kaushiki parmar");
+let a=10;
+let b=20;
+console.log("sum is:", a+b);
+let pencil=20;
+let erasor=30;
+let output=`the total price is: ${pencil+erasor} rupees`;
+console.log(output);
