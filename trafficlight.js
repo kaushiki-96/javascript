38cd734f1b121efb1d71edf93c3a4d63eac9d454
kaushiki-96 <kaushiki251996@gmail.com>
2023-12-14 22:35:40 +0530

    new commit

---
+++ trafficlight.js
@@ -0,0 +1,13 @@
+let color="green";
+if(light color=="red")
+{
+    console.log("stop light is red");
+}
+if(light color=="yellow")
+{
+    console.log("wait the color is yellow");
+}
+if(light color=="green")
+{
+    console.log("go!light color is green");
+}
