c92601c4dcfa9ac0694285432c1f3de5bfe4030e
kaushiki-96 <kaushiki251996@gmail.com>
2023-12-14 22:24:15 +0530

    new commit

---
+++ new.js
@@ -0,0 +1,11 @@
+//conditional statements
+console.log("before if statement");
+let age=23;
+if(age>=18)
+{
+    console.log("you can vote");
+}
+let firstName=kaushiki;
+if(firstName==kaushiki){
+    console.log(`welcome${firstName}`);
+}
