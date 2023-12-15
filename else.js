66e5573dafa69568c89c53d3501d372f1eb53f02
kaushiki-96 <kaushiki251996@gmail.com>
2023-12-15 20:18:34 +0530

    new commit

---
+++ else.js
@@ -0,0 +1,16 @@
+let color="blue";
+if(color==="red")
+{
+    console.log("stop");
+}
+elseif(color==="yellow")
+{
+    console.log("wait");
+}
+elseif(color==="green")
+{
+    console.log("go");
+}
+else{
+    console.log("your traffic light is broken");
+}
---
+++ elseif.js
@@ -0,0 +1,12 @@
+let marks =75;
+if(marks>=80){
+    console.log("you get an A");
+}
+elseif(marks>=50)
+{
+    console.log("you get B")
+}
+elseif(marks<30)
+{
+    console.log("you get C");
+}
