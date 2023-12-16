960baf8bfe0ef50cb68fef7e9259eb5e5c526b4a
kaushiki-96 <kaushiki251996@gmail.com>
2023-12-16 20:10:27 +0530

    new commit

---
+++ practice.js
@@ -0,0 +1,19 @@
+
+//practice1//
+if(num%10==0)
+{
+    console.log("good");
+}
+else{
+    console.log("bad");
+}
+//practice2//
+let name=prompt("enter name");
+let age=prompt("enter age");
+alert(`${name} is ${age} years old `);
+//practice3//
+let str="apple";
+if((str[0]=='a'&& str[0]=='A')|| (str.length>5))
+{
+    console.log("its golden");
+}
