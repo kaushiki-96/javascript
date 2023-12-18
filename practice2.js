9e27ea3314c6444df4ca3b9a03b065997bc73c6c
kaushiki-96 <kaushiki251996@gmail.com>
2023-12-18 23:16:35 +0530

    new commit

---
+++ practice2.js
@@ -0,0 +1,16 @@
+if(a>b){
+    if(a>c){
+        console.log("a is largest");
+    }
+    else{
+        console.log("c is largest");
+    }
+}
+else{
+    if(b>c){
+        console.log("b is largest");
+    }
+    else{
+        console.log("c is largest");
+    }
+}
