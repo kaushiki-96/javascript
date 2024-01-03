f3c9744c9133d4e438b1242379e30a20c18f91c4
kaushiki-96 <kaushiki251996@gmail.com>
2024-01-03 23:07:50 +0530

    new commit

---
+++ forloop.js
@@ -0,0 +1,15 @@
+for(let i=1;i<=5;i++){
+    console.log(i);
+}
+//practice print odd nos from 1 to 15//
+for(let k=1;k<=15;i=i+2){
+    console.log(k);
+}
+//print all even nos from 1 to 20//
+for(let i=2;i<=20;i=i+2){
+    console.log(i);
+}
+//table of 5//
+for(let j=5;j<=50;j=j+5){
+    console.log(j);
+}
