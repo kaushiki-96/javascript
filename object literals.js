bb1e3607a94cdbb6f647117ab9a8307f29068781
kaushiki-96 <kaushiki251996@gmail.com>
2024-01-05 21:05:27 +0530

    new commit

---
+++ object literals.js
@@ -0,0 +1,14 @@
+let student={
+    name:"kaushiki",
+    age:27,
+    country:"india",
+    city:"delhi"
+};
+//create object literal for twitter post//
+const post={
+    username:"@kaushiki",
+    content:"this is my first post",
+    likes:50,
+    repost:20,
+    tags:["@kaushiki1","@abc"]
+};
