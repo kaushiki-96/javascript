98809bbfaf4b1842c0f1c338ed0fbe66ac52c2fb
kaushiki-96 <kaushiki251996@gmail.com>
2024-01-11 21:31:49 +0530

    new commit

---
+++ objectofobjects.js
@@ -0,0 +1,14 @@
+const classinfo={
+    aman:{
+        grade:"A",
+        class:"ninth"
+    },
+    akriti:{
+        grade:"b",
+        class:"ninth"
+    },
+    shubham:{
+        grade:"c",
+        class:"ninth"
+    }
+};
